2be718745faedb17f9256c7e8051e12254d205bd
Palak Goyal <palak2015.be22@chitkara.edu.in>
2026-07-01 19:41:57 +0530

    updated changes

--- src/main/java/com/coatcard/axon/config/RedisConfig.java
+++ src/main/java/com/coatcard/axon/config/RedisConfig.java
@@ -1,14 +1,58 @@
 package com.coatcard.axon.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfig {
+
+    private final Environment env;
+
+    public RedisConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    @ConditionalOnProperty(name = "spring.data.redis.url", havingValue = "", matchIfMissing = true)
+    public RedisServer embeddedRedisServer() throws IOException {
+        return new RedisServer(6379);
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() throws URISyntaxException {
+        String redisUrl = env.getProperty("spring.data.redis.url");
+        if (redisUrl == null || redisUrl.isBlank()) {
+            RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+            return new LettuceConnectionFactory(config);
+        }
+
+        URI uri = new URI(redisUrl);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(uri.getHost(), uri.getPort());
+        if (uri.getUserInfo() != null && uri.getUserInfo().contains(":")) {
+            config.setPassword(RedisPassword.of(uri.getUserInfo().split(":", 2)[1]));
+        }
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
